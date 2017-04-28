67c991a51ed8a206f3cbeafec93e8035995af786
Anders Storm Hansen <ash@bruun-rasmussen.dk>
2017-04-28 11:55:55 +0200

    Remove superflous access modifiers in interface

--- src/main/java/dk/es/br/dibs/DibsResponse.java
+++ src/main/java/dk/es/br/dibs/DibsResponse.java
@@ -9,17 +9,17 @@ public interface DibsResponse {
     /**
      * @return <code>true</code> of the check validated the card
      */
-    public boolean isValid();
+    boolean isValid();
     /**
      * @return {@link String} representation of the error code (if any)
      */
-    public String getReason();
+    String getReason();
     /**
      * @return {@link String} representation of the error action code (if any)
      */
-    public String getActionCode();
+    String getActionCode();
     /**
      * @return {@link String} representation of the transaction ID (if any)
      */
-    public Long getTransactionId();
+    Long getTransactionId();
 }
