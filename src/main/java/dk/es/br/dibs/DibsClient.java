eea6b5f86a86f8d0e740d2c3af4cfaa1a7b90e70
ash <ash@bruun-rasmussen.dk>
2019-05-23 16:45:19 +0200

    Do not add fullreply param as semantics are not clear from DIBS yet

--- src/main/java/dk/es/br/dibs/DibsClient.java
+++ src/main/java/dk/es/br/dibs/DibsClient.java
@@ -253,7 +253,6 @@ public class DibsClient
     msg.put("currency", codeOf(currency));
     msg.put("capturenow", "yes");
     msg.put("uniqueoid", "yes");
-    msg.put("fullreply", "1");
 
     // cf. http://tech.dibspayment.com/D2/FlexWin/API/MD5
     String md5key = md5of("merchant=" + getMerchantId() + "&orderid=" + orderId + "&ticket=" + accountId + "&currency=" + codeOf(currency) + "&amount=" + cents);
